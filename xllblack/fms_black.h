b0a4c6aba16fd044b91f4145cba57869eddc861d
LZ2735 <55263722+LZ2735@users.noreply.github.com>
2019-10-03 04:41:11 -0400

    lz2735_hw4

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -3,8 +3,22 @@
 #include "fms_normal.h"
 
 namespace fms::black {
-
-    //!!! Black put and call code goes here.
-    //!!! Use the functions in fms_normal.h
-
+	//!!! Black put and call code goes here.
+	//!!! Use the functions in fms_normal.h
+	inline double put(double f, double sigma, double k, double t) {
+		double nominator = log(f / k) + (t * sigma * sigma / 2);
+		double denominator = sigma * sqrt(t);
+		double d1 = nominator / denominator;
+		double d2 = d1 - (sigma * sqrt(t));
+		double p = (k * fms::normal::cdf(-d2)) - (f * fms::normal::cdf(-d1));
+		return p;
+	}
+	inline double call(double f, double sigma, double k, double t) {
+		double nominator = log(f / k) + (t * sigma * sigma / 2);
+		double denominator = sigma * sqrt(t);
+		double d1 = nominator / denominator;
+		double d2 = d1 - (sigma * sqrt(t));
+		double c = (f * fms::normal::cdf(d1)) - (k * fms::normal::cdf(d2));
+		return c;
+	}
 } // fms::black
